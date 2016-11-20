5c29dcbe330a133c85977e3cadbf0e5a6daf4faa
aefaw <alli.efaw@gmail.com>
2016-11-20 03:04:22 -0500

    kjh

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,8 @@
 document.write("<div class='nav'>");
 
-document.write("<a href='/'>HOME</a>");
-document.write("<a href='/'>ABOUT</a>");
+document.write("<a href='/index.html'>HOME</a>");
+document.write("<a href='/about.html'>ABOUT</a>");
+document.write("<a href='/contact.html'>CONTACT</a>");
 
 document.write("</div>");
 
