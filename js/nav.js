8b1ee8f96deb71542765726cb2f3c29e1dd88dc7
aefaw <alli.efaw@gmail.com>
2016-11-28 09:10:25 -0500

    oops

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,7 @@ document.write("<div class='nav'>");
 document.write("<a href='/index.html'>HOME</a>");
 document.write("<a href='/about.html'>ABOUT</a>");
 document.write("<a href='/contact.html'>CONTACT</a>");
+document.write("<a href='http://jodemu.tumblr.com'>BLOG</a>");
 
 document.write("</div>");
 
