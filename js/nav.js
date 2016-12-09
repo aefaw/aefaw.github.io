1c4e42b1aba43847a0c7ff1bd119fa43c7e994c2
aefaw <alli.efaw@gmail.com>
2016-12-08 21:11:45 -0500

    ???

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,7 @@
 document.write("<div class='nav'>");
 
 document.write("<a href='/index.html'>HOME</a>");
-document.write("<a href='/about.html'>ABOUT</a>");
-document.write("<a href='/contact.html'>CONTACT</a>");
+document.write("<a href='/about.html'>ABOUT/CONTACT</a>");
 document.write("<a href='http://jodemu.tumblr.com'>BLOG</a>");
 
 document.write("</div>");
