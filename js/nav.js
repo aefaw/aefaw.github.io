44b9cdf8df922fff6a43d7fd9d7693a9b206678f
aefaw <alli.efaw@gmail.com>
2017-01-12 01:54:20 -0500

    image fixes

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 document.write("<div class='nav'>");
 
 document.write("<a href='/index.html'>HOME</a>");
+document.write("<a href='/resume.html'>RESUME</a>");
 document.write("<a href='/about.html'>ABOUT/CONTACT</a>");
 document.write("<a href='http://jodemu.tumblr.com'>BLOG</a>");
 
