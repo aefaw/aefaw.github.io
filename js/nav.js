b37ae3776067883b7049b690b601894f23ecd4b4
aefaw <alli.efaw@gmail.com>
2017-01-25 11:36:25 -0500

    ayo

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,8 @@ document.write("<div class='nav'>");
 
 document.write("<a href='/index.html'>HOME</a>");
 document.write("<a href='/resume.html'>RESUME</a>");
-document.write("<a href='/about.html'>ABOUT/CONTACT</a>");
+document.write("<a href='/contact.html'>CONTACT</a>");
+document.write("<a href='/about.html'>ABOUT</a>");
 document.write("<a href='http://jodemu.tumblr.com'>BLOG</a>");
 
 document.write("</div>");
